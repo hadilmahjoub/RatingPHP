172a224f7b4a08cb35d6b5792224ba6b846de7c7
hadil mahjoub <hadil.mahjoub@gmail.com>
2022-04-14 01:09:05 +0200

    updated php

--- index.php
+++ index.php
@@ -1,25 +1,32 @@
-<?php 
-    $po = isset($_POST["rate"]);
-    $cook = isset($_COOKIE["rate"]);
+<?php
+$po = isset($_POST["rate"]);
+$cook = isset($_COOKIE["rate"]);
+?>
 
+<html>
+<link rel="stylesheet" href="style.css">
+<div class="container">
+<!--Container-->
+
+    <?php
     if ($po) {
         if (!$cook) {
-            setcookie('rate',$_POST["rate"], time()+120);
-?>
-        <div class="accept"><?= "Evaluation bien reçu!" ?></div>
-<?php 
-    } else {
-?>
-        <div class="reject">
-            <?= "Désolé vous avez déjà évalué! C'est le contenu de votre réponse:".$_COOKIE["rate"] ?>
-            <!-- <?php print_r($_COOKIE)?> -->
-        </div>
-<?php 
-        }
-        
-    }
-?>
+            setcookie('rate', $_POST["rate"], time() + 120);
+    ?>
+            <div class="accept"><?= "Evaluation bien reçu!" ?></div>
+        <?php
+        } else {
+        ?>
+            <div class="reject">
+                <?= "Désolé vous avez déjà évalué! C'est le contenu de votre réponse: <b>" . $_COOKIE["rate"] . "</b>" ?>
+                <!-- <?php print_r($_COOKIE) ?> -->
+            </div>
+            
+</div>
+<!--Container-->
 
-
-
-    
+</html>
+<?php
+        } // End If Else (interne)
+    } // End If (externe)
+?>
